dc2ddfbdf7b01647320c62a915ec61c3f76f5bed
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-05-07 17:07:01 +0100

    Make levels wide again

--- src/main/java/cs4303/p4/map/Level.java
+++ src/main/java/cs4303/p4/map/Level.java
@@ -83,7 +83,7 @@ public class Level {
             e.printStackTrace();
         }
 
-        Level.gridWidth = (p.width / cellSize);// * Math.max(width, 2);
+        Level.gridWidth = (p.width / cellSize); * Math.max(width, 2);
         Level.gridHeight = (Constants.Screen.height - Constants.Screen.GamePlay.infoPanelHeight) / cellSize;
 
         levelGrid = new Tile[gridHeight][gridWidth];
